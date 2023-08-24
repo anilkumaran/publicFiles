706ddda0272cf9b6e0563c6a630922107c6851f6
Anil Koppula <72085415+anilkumaran@users.noreply.github.com>
2023-08-24 17:54:01 +0530

    Update DBHandler.py

--- DBHandler.py
+++ DBHandler.py
@@ -1,1 +1,75 @@
-print("Hello DB Service")
+import pymysql
+
+ 
+
+class DB_Handler:
+
+    def __init__(self):
+
+        self.con=None
+
+        self.__connect(True)
+
+ 
+
+    def __connect(self, force=False):
+
+        self.con = pymysql.connect(host='172.16.1.210',
+
+                                    user='bbenjaram',
+
+                                    password='Sid$osi#123',
+
+                                    database='bbenjaram',
+
+                                    cursorclass=pymysql.cursors.DictCursor)
+
+       
+
+    def run_query(self,stmt,values=()):
+
+        result = None
+
+        with self.con:
+
+            with self.con.cursor() as cursor:
+
+                # selecting record or records
+
+                cursor.execute(stmt,values)
+
+                result = cursor.fetchall()
+
+        return result
+
+           
+
+    def run_dml(self,stmt,values=()):
+
+        with self.con:
+
+            with self.con.cursor() as cursor:
+
+                # selecting record or records
+
+                cursor.execute(stmt,values)
+
+            self.con.commit()
+
+            print("Operation successful")
+
+           
+
+    def run_ddl(self,stmt):
+
+        with self.con:
+
+            with self.con.cursor() as cursor:
+
+                # selecting record or records
+
+                cursor.execute(stmt)
+
+            self.con.commit()
+
+            print("Operation done")
