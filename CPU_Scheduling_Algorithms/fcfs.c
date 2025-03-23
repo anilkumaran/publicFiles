edbc09f5532228e4e9fce3af64c681c42b438cec
Anil Koppula <72085415+anilkumaran@users.noreply.github.com>
2025-03-23 22:23:21 +0530

    Update fcfs.c

--- CPU_Scheduling_Algorithms/fcfs.c
+++ CPU_Scheduling_Algorithms/fcfs.c
@@ -53,31 +53,43 @@ void sortByArrivalTime(struct Process proc[], int n) {
         }
     }
 }
+int calculate_times(struct Process p[], int i, int timer){
+    p[i].CT = timer + p[i].BT;
+    p[i].TAT = p[i].CT - p[i].AT;
+    p[i].WT = p[i].TAT - p[i].BT;
+    timer += p[i].BT;
+    return timer;
+}
+
 void fcfs(struct Process p[], int n){
     // bool completed = false;
     int timer = p[0].AT;
     printf("\n");
     for(int i=0; i<n; i++) {
-        // if (i == n-1) {
-        //     struct Process next_proc = p[i];
-        // } else {
-        //     struct Process next_proc = p[i+1];
-        // }
-        // printf("\nSTART i=%d, p[i].PID=%d, p[i].AT=%d, timer=%d\n", i, p[i].PID, p[i].AT, timer);
+        printf("\n-------------------------");
+        printf("\nSTART i=%d, p[i].PID=%d, p[i].AT=%d, p[i].CT=%d, timer=%d\n", i, p[i].PID, p[i].AT, p[i].CT, timer);
         if(p[i].AT <= timer) {
             printf("\nIn if\n");
-            p[i].CT = timer + p[i].BT;
-            p[i].TAT = p[i].CT - p[i].AT;
-            p[i].WT = p[i].TAT - p[i].BT;
-            timer += p[i].BT;
+            timer = calculate_times(p, i, timer);
         } else {
             printf("\nIn else\n");
-            timer += 1;
+            while (p[i].AT > timer) {
+                timer += 1;
+            }
+            timer = calculate_times(p, i, timer);
         }
-        // printf("END i=%d, p[i].PID=%d, p[i].AT=%d, timer=%d\n", i, p[i].PID, p[i].AT, timer);
+        printf("END i=%d, p[i].PID=%d, p[i].AT=%d, p[i].CT=%d, timer=%d\n", i, p[i].PID, p[i].AT, p[i].CT, timer);
+        printf("\n-------------------------");
     }
 }
 
+float get_avg_WT(struct Process p[], int n) {
+    float sum_WT = 0;
+    for(int i=0; i<n; i++) {
+        sum_WT += p[i].WT;
+    }
+    return sum_WT/n;
+}
 
 int main() {
     // int n;
@@ -91,28 +103,38 @@ int main() {
     //     printf("Enter BT of process %d: ", proc[i].PID);
     //     scanf("%d", &proc[i].BT);
     // }
-    struct Process proc[5] = {
-        {1, 2, 6,  0, 0, 0},
-        {2, 5, 3,  0, 0, 0},
-        {3, 1, 8,  0, 0, 0},
-        {4, 0, 3,  0, 0, 0},
-        {5, 4, 4,  0, 0, 0}
-    };
+    // 1
+    // int n = 5;
+    // struct Process proc[5] = {
+    //     {1, 20,	2,  0, 0, 0},
+    //     {2, 30,	2,  0, 0, 0},
+    //     {3, 31,	2,  0, 0, 0},
+    //     {4, 10,	2,  0, 0, 0},
+    //     {5, 50,	2,  0, 0, 0}
+    // };
 
-    int n = 5;
+    int n = 3;
+    struct Process proc[5] = {
+        {1, 0,	10,  0, 0, 0},
+        {2, 0,	5,  0, 0, 0},
+        {3, 0,	5,  0, 0, 0}
+    };
 
     // printf("Before Sorting:\n");
     // display(proc, n);
-
     sortByArrivalTime(proc, n);
-
     printf("\nAfter Sorting by Arrival Time:\n");
     display(proc, n);
 
     fcfs(proc, n);
+    printf("\n\nAfter FCFS:\n");
+    display(proc, n);
+    
     sortByProcessId(proc, n);
-    printf("\n\nAfter FCFS - Sort by PID:\n");
+    printf("\n\nAfter FCFS with PID sort:\n");
     display(proc, n);
 
+    float avg_WT = get_avg_WT(proc, n);
+    printf("\nAVG WT=%.2f", avg_WT);
     return 0;
 }
