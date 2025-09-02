bec0cb2a5b7bb185fc7b6bf8b5c871186ca77061
Anil Koppula <72085415+anilkumaran@users.noreply.github.com>
2025-09-02 14:56:37 +0530

    Update patterns.py

--- python_programs/patterns.py
+++ python_programs/patterns.py
@@ -1,16 +1,11 @@
 rows = 5
-
 for i in range(rows):
-    # spaces
     for j in range(rows-i-1):
-        # Change the first and last empty strings to spaces to get different patterns
         print(" ", end = "")
-    # stars
     print("*"*(2*i+1))
 
-# For Rhombus or Diamond
-for row in range(rows-1, 0, -1):
-    # spaces
-    print(""*(rows-row), end="")
-    # stars
-    print("*"*(2*row-1))
+#For Rhombus or Diamond
+for m in range(rows):
+    for n in range(m+1):
+        print(" ", end = " ")
+    print("*"*(2*(rows-1-m)-1))
